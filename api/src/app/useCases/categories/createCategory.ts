55e3707af6e275f33facf6d8dc110d28f1ca4497
José Lopes <946393361bill@gmail.com>
2023-11-08 16:54:05 +0000

    implementing trycatch at createCategory

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -2,10 +2,16 @@ import { Request, Response } from "express";
 
 import { Category } from "../../models/Category";
 
-export async function createCategories(req: Request, res: Response){
-	const { icon, name } = req.body;
+export async function createCategories(req: Request, res: Response) {
+	try {
+		const { icon, name } = req.body;
 
-	const category = await Category.create({ icon, name })
+		const category = await Category.create({ icon, name })
 
-	res.json(category);
+		res.status(201).json(category); // o 201 representa que o recurso solicitado foi criado
+
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 };
