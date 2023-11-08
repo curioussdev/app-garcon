4998349a4b0e7193c5363f23d1b926310767a897
José Lopes <946393361bill@gmail.com>
2023-11-08 13:32:35 +0000

    model creation completed

---
+++ api/src/app/models/Order.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+
+export const Order = model('Order', new Schema({
+	table: {
+		type: String,
+		required: true
+	},
+	status: {
+		type: String,
+		enum: ['WAITNG', 'IN_PRODUCTION', 'DONE'], // O enums erve para cada pedido atender apenas aos três estados citados no Array
+		default: 'WAITING' // o estado inicial de toos os pedidos deve ser o mencionado no default
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	products:{
+		required: true,
+		type: [{
+			product: {
+				type: Schema.Types.ObjectId,
+				required: true,
+				ref: 'Product',
+			},
+			quatity: {
+				type: Number,
+				default: 1,
+			}
+		}]
+	}
+}));
--- api/src/app/models/Product.ts
+++ api/src/app/models/Product.ts
@@ -1,4 +1,4 @@
-import { model, Schema} from 'mongoose';
+import { model, Schema } from 'mongoose';
 
 export const Product = model('Product', new Schema({
 	name: {
@@ -8,5 +8,32 @@ export const Product = model('Product', new Schema({
 	description: {
 		type: String,
 		required: true
+	},
+	imagePath: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	ingredients: {
+		required: true,
+		type: [{
+			name: {
+				type: String,
+				reuqired: true
+			},
+			icon: {
+				type: String,
+				required: true
+			}
+		}]
+	},
+	category: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		ref: 'Category',  // Faz referência ao model 'Category'
 	}
+
 }));
