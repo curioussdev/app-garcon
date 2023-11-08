eaec192d374afc850d8c89f62ce05106039868b7
José Lopes <946393361bill@gmail.com>
2023-11-08 21:39:26 +0000

    createProduct & listProduct DONE

--- api/src/app/useCases/products/createProduct.ts
+++ api/src/app/useCases/products/createProduct.ts
@@ -4,9 +4,19 @@ import { Product } from "../../models/Product";
 
 export async function createProduct(req: Request, res: Response) {
 	try {
+		const imagePath = req.file?.filename; // só acess ao filename se existir o file
+		const { name, description, price, category, ingredients } = req.body;
 
-		console.log(req.file)
-console.log(req.body)
+		const product = await Product.create({
+			name,
+			imagePath,
+			description,
+			price: Number(price),
+			category,
+			ingredients: JSON.parse(ingredients)
+		});
+
+		res.status(201).json(product)
 	} catch (error) {
 		console.log(error);
 		res.sendStatus(500);
