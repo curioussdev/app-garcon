8743ba77dc79a975ff6c2b8678a7d74dd0d667f0
José Lopes <946393361bill@gmail.com>
2023-11-28 00:05:52 +0000

    testing fech from fake data

--- front/src/components/OrdersBoard/index.tsx
+++ front/src/components/OrdersBoard/index.tsx
@@ -1,31 +1,34 @@
-import {  Board, OrdersContainer } from './styles'
+import { Order } from '../../types/order';
+import { Board, OrdersContainer } from './styles'
 
 interface OrderBoardProps {
   icon: string;
   title: string;
+  orders: Order[];
 }
 
-export function OrdersBoard({icon, title}: OrderBoardProps){
+export function OrdersBoard({ icon, title, orders }: OrderBoardProps) {
 
-  return(
+  return (
     <Board>
-        <header>
-          <span>{icon}</span>
-          <strong>{title}</strong>
-          <span>(1)</span>
-        </header>
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>{orders.length}</span>
+      </header>
 
+
+      {orders.length > 0 && (
         <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens </span>
+        {orders.map((orders) => (
+          <button type="button" key={orders._id}>
+            <strong>Mesa {orders.table}</strong>
+            <span>{orders.products.length} itens</span>
           </button>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens </span>
-          </button>
-        </OrdersContainer>
-      </Board>
+        ))}
+      </OrdersContainer>
+      )}
+    </Board>
   )
 }
 
