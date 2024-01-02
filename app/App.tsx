6960995eba042d2921dcdd2f931e91b8ca56d1f8
José Lopes <946393361bill@gmail.com>
2024-01-02 23:34:41 +0000

    setup editorconfig and eslint

--- app/App.tsx
+++ app/App.tsx
@@ -2,20 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>LET STUDY</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>HORA DE CODAR</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
 
-  },
+    },
 });
